e4e66fffc7e7f83927f6230e638c441133bfb914
digerati-red <brettkingsley@gmail.com>
2025-04-08 11:40:56 -0600

    Ruff edit

--- salmon/tagger/combine.py
+++ salmon/tagger/combine.py
@@ -141,12 +141,17 @@ def combine_tracks(base, meta):
                 raise TrackCombineError(f"Disc {disc} track {num} does not exist.") from None
 
             # Use unidecode comparison when there are accents in the title
-            if (re_strip(unidecode(track["title"])) != re_strip(unidecode(btrack["title"]))
-                    and btrack["title"] is not None):
-                # Allow replacement if the base title is part of the meta title (e.g., remix scenario)
-                if (btrack["title"] and track["title"]
-                        and re_strip(unidecode(btrack["title"])) in re_strip(unidecode(track["title"]))):
-                    btrack["title"] = track["title"]
+            if (
+                    re_strip(unidecode(track["title"])) != re_strip(unidecode(btrack["title"]))
+                    and btrack["title"] is not None
+            ) and (
+                    btrack["title"]
+                    and track["title"]
+                    and re_strip(unidecode(btrack["title"])) in re_strip(unidecode(track["title"]))
+            ):
+
+            # Allow replacement if the base title is part of the meta title (e.g., remix scenario)
+                btrack["title"] = track["title"]
 
             if btrack["title"] is None:
                 btrack["title"] = track["title"]
