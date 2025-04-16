17e68ec8878183715b388fcef9965a519bb7e33c
Audionut <audionut11@gmail.com>
2025-04-16 23:16:51 +1000

    cleaner windows handling code

--- salmon/common/__init__.py
+++ salmon/common/__init__.py
@@ -51,17 +51,12 @@ class Prompt:
 
     async def __call__(self, msg, end="\n", flush=False):
         if not self.reader_added:
+            print(msg, end=end, flush=flush)
             try:
                 loop.add_reader(sys.stdin, self.got_input)
                 self.reader_added = True
             except NotImplementedError:
-                print(msg, end=end, flush=flush)
                 return input()
-
-        if self.reader_added:
-            print(msg, end=end, flush=flush)
-            return (await self.q.get()).rstrip("\n")
-        return ""
 
 
 prompt_async = Prompt()
