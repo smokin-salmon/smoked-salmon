da33339495360d90ffab0f9e8f4b3ad2671741fa
redusys <redusys.ops@gmail.com>
2025-06-02 09:22:50 +0200

    Support bandcamp single tracks URLs

--- salmon/sources/bandcamp.py
+++ salmon/sources/bandcamp.py
@@ -6,9 +6,10 @@ from salmon.sources.base import BaseScraper
 class BandcampBase(BaseScraper):
 
     search_url = "https://bandcamp.com/search/"
-    regex = re.compile(r"^https?://([^/]+)/album/([^/]+)/?")
-    release_format = "https://{rls_url}/album/{rls_id}"
+    regex = re.compile(r"^https?://([^/]+)/(album|track)/([^/]+)/?")
+    release_format = "https://{rls_url}/{type}/{rls_id}"
 
     @classmethod
     def format_url(cls, rls_id, rls_name=None):
-        return cls.release_format.format(rls_url=rls_id[0], rls_id=rls_id[1])
+        # rls_id is now expected to be a tuple of (domain, type, id)
+        return cls.release_format.format(rls_url=rls_id[0], type=rls_id[1], rls_id=rls_id[2])
