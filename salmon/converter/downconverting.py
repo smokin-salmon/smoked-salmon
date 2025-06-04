32e54f51993eab79b2a38b3beee466b5843fffb9
KyokoMiki <161209740+KyokoMiki@users.noreply.github.com>
2025-06-05 07:46:09 +0800

    Merge transcoding and downconverting module from smoked-salmon-oasis

--- salmon/converter/downconverting.py
+++ salmon/converter/downconverting.py
@@ -1,7 +1,5 @@
-import contextlib
 import os
 import re
-import shlex
 import subprocess
 import time
 from copy import copy
@@ -14,19 +12,30 @@ from salmon.errors import InvalidSampleRate
 from salmon.tagger.audio_info import gather_audio_info
 
 THREADS = [None] * config.SIMULTANEOUS_THREADS
-COMMAND = "sox {input_} -G -b 16 {output} rate -v -L {rate} dither"
 FLAC_FOLDER_REGEX = re.compile(r"(24 ?bit )?FLAC", flags=re.IGNORECASE)
 
 
-def convert_folder(path):
+def convert_folder(path, override_sample_rate=None):
+
     new_path = _generate_conversion_path_name(path)
+    if override_sample_rate:
+        new_path = re.sub(
+            "FLAC",
+            f"24-{override_sample_rate / 1000:.0f}",
+            new_path,
+            flags=re.IGNORECASE,
+        )
     if os.path.isdir(new_path):
         return click.secho(
             f"{new_path} already exists, please delete it to re-convert.", fg="red"
         )
 
     files_convert, files_copy = _determine_files_actions(path)
-    _convert_files(path, new_path, files_convert, files_copy)
+    sample_rate = _convert_files(
+        path, new_path, files_convert, files_copy, override_sample_rate
+    )
+
+    return sample_rate, new_path
 
 
 def _determine_files_actions(path):
@@ -53,7 +62,9 @@ def _generate_conversion_path_name(path):
     return os.path.join(os.path.dirname(path), foldername)
 
 
-def _convert_files(old_path, new_path, files_convert, files_copy):
+def _convert_files(
+    old_path, new_path, files_convert, files_copy, override_sample_rate=None
+):
     files_left = len(files_convert) - 1
     files = iter(files_convert)
 
@@ -63,52 +74,73 @@ def _convert_files(old_path, new_path, files_convert, files_copy):
         copyfile(file_, output)
         click.secho(f"Copied {os.path.basename(file_)}")
 
-    while True:
+    converting = True
+    while converting:
+        converting = False
         for i, thread in enumerate(THREADS):
-            if thread and thread.poll() is not None:  # Process finished
-                exit_code = thread.returncode
-                if exit_code != 0:  # Error handling
-                    stderr_output = thread.communicate()[1].decode("utf-8", "ignore")
-                    click.secho(f"Error downconverting a file, error {exit_code}:", fg="red")
-                    click.secho(stderr_output)
-                    raise click.Abort  # Consider collecting errors instead of aborting
+            if thread and thread.poll() is not None:
+                if thread.poll() != 0:
+                    click.secho(
+                        f"Error downconverting a file, error {thread.poll()}:", fg="red"
+                    )
+                    click.secho(thread.communicate()[1].decode("utf-8", "ignore"))
+                    raise click.Abort
+                try:
+                    thread.kill()
+                except:  # noqa: E722
+                    pass
 
-                # Process is finished, and there was no error
-                THREADS[i] = None  # Mark the slot as free
-
-            if THREADS[i] is None:  # If thread is free, assign new file
+            if not thread or thread.poll() is not None:
                 try:
                     file_, sample_rate = next(files)
                 except StopIteration:
-                    THREADS[i] = None
-                else:
-                    output = file_.replace(old_path, new_path)
-                    THREADS[i] = _convert_single_file(file_, output, sample_rate, files_left)
-                    files_left -= 1
+                    break
 
-        if all(t is None for t in THREADS):     # No active threads and no more files
-            break
+                output = file_.replace(old_path, new_path)
+                THREADS[i] = _convert_single_file(
+                    file_, output, sample_rate, files_left, override_sample_rate
+                )
+                files_left -= 1
+            converting = True
         time.sleep(0.1)
 
+    return override_sample_rate if override_sample_rate else _get_final_sample_rate(sample_rate)
 
-def _convert_single_file(file_, output, sample_rate, files_left):
+
+def _convert_single_file(
+    file_, output, sample_rate, files_left, override_sample_rate=None
+):
     click.echo(f"Converting {os.path.basename(file_)} [{files_left} left to convert]")
     _create_path(output)
-    command = COMMAND.format(
-        input_=shlex.quote(file_),
-        output=shlex.quote(output),
-        rate=_get_final_sample_rate(sample_rate),
-    )
-    return subprocess.Popen(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
-    )
+
+    command = [
+        "sox",
+        file_,
+        "-R",
+        "-G",
+        *(["-b", "16"] if not override_sample_rate else []),
+        output,
+        "rate",
+        "-v",
+        "-L",
+        (
+            str(override_sample_rate)
+            if override_sample_rate
+            else str(_get_final_sample_rate(sample_rate))
+        ),
+        "dither",
+    ]
+
+    return subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 def _create_path(filepath):
     p = os.path.dirname(filepath)
     if not os.path.isdir(p):
-        with contextlib.suppress(FileExistsError):
+        try:
             os.makedirs(p)
+        except FileExistsError:
+            pass
 
 
 def _get_final_sample_rate(sample_rate):
@@ -117,3 +149,23 @@ def _get_final_sample_rate(sample_rate):
     elif sample_rate % 48000 == 0:
         return 48000
     raise InvalidSampleRate
+
+
+def generate_conversion_description(url, sample_rate):
+
+    description = ""
+
+    if sample_rate <= 48000:
+        description += (
+            f"Encode Specifics: 16 bit {sample_rate / 1000:.01f} kHz\n"
+            f"[b]Source:[/b] {url}\n"
+            f"[b]Transcode process:[/b] [code]sox input.flac -R -G -b 16 output.flac rate -v -L {sample_rate} dither[/code]\n"
+        )
+    else:
+        description += (
+            f"Encode Specifics: 24 bit {sample_rate / 1000:.01f} kHz\n"
+            f"[b]Source:[/b] {url}\n"
+            f"[b]Transcode process:[/b] [code]sox input.flac -R -G output.flac rate -v -L {sample_rate} dither[/code]\n"
+        )
+
+    return description
